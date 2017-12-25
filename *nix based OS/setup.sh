82683ebc3cbc74a3f26d1caf4259d25ce1959b0f
tobiloba23 <tobiloba.ades@gmail.com>
2017-12-25 20:54:16 +0100

    Updated the setup shell script

--- *nix based OS/setup.sh
+++ *nix based OS/setup.sh
@@ -1,19 +1,43 @@
 #!/bin/sh
 
+homebrew=`brew -v`
+
 node=`node -v`
 if [ -z "$node" ]; then 
-brew install node
-fi
-
-mongodb=`mongo --version`
-if [ -z "$mongodb" ]; then 
-brew install mongodb
+    if [ -n "$homebrew" ]; then
+        brew install node
+    else
+        echo "Kindly install homebrew to proceed"
+        exit
+    fi
 fi
 
 docker=`docker -v`
 if [ -z "$docker" ]; then 
-brew cask install docker
+    if [ -n "$homebrew" ]; then
+        brew cask install docker
+    else
+        echo "Kindly install homebrew to proceed"
+        exit
+    fi
 fi
+
+open -a docker
+
+mongodb=`mongo --version`
+if [ -z "$mongodb" ]; then 
+    if [ -z "$homebrew" ]; then
+        brew install mongodb
+    else
+        echo "Kindly install homebrew to proceed"
+    fi
+fi
+
+dockerI=`docker info`
+while [ -z "$dockerI" ]
+do
+    dockerI=`docker info`
+done
 
 jasmine=`jasmine -v`
 if [ -z "$jasmine" ]; then 
